1d680ad384b07bf70f1a229e20e4fe4ea09afb1c
TavarishYosef <yusufkaratas22@ku.edu.tr>
2023-05-09 18:28:52 +0300

    Pledge of Honor

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,3 +1,15 @@
+/************** Pledge of Honor ******************************************
+I hereby certify that I have completed this programming project on my own 
+without any help from anyone else. The effort in the project thus belongs 
+completely to me. I did not search for a solution, or I did not consult any 
+program written by others or did not copy any program from other sources. I 
+read and followed the guidelines provided in the project description.
+
+READ AND SIGN BY WRITING YOUR NAME SURNAME AND STUDENT ID
+
+SIGNATURE: <Yusuf Cemal Karatas, 83639>
+*************************************************************************/
+
 package main;
 
 public class Main {
