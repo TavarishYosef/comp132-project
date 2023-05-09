3bafec717c6ab9d049c971a984a7bab25a24af1b
TavarishYosef <yusufkaratas22@ku.edu.tr>
2023-05-09 23:19:49 +0300

    LoginPage

---
+++ src/gui/LoginPage.java
@@ -0,0 +1,61 @@
+package gui;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class LoginPage extends JFrame {
+	
+    private JTextField nicknameField;
+    private JPasswordField passwordField;
+
+    public LoginPage() {
+        // Set up the JFrame
+        setTitle("PhotoCloud Login Page2");
+        setSize(300, 200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // Create the nickname label and text field
+        JLabel nicknameLabel = new JLabel("Nickname:");
+        nicknameField = new JTextField(20);
+
+        // Create the password label and password field
+        JLabel passwordLabel = new JLabel("Password:");
+        passwordField = new JPasswordField(20);
+
+        // Create the login button
+        JButton loginButton = new JButton("Login");
+        loginButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TODO: Validate the user's credentials and navigate to the profile or discover page
+            }
+        });
+
+        // Create the Signup link
+        JLabel signupLabel = new JLabel("Don't have an account? ");
+        JButton signupButton = new JButton("Signup");
+        signupButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TODO: Navigate to the Signup page
+            }
+        });
+
+        // Add the components to the JFrame
+        JPanel panel = new JPanel();
+        panel.add(nicknameLabel);
+        panel.add(nicknameField);
+        panel.add(passwordLabel);
+        panel.add(passwordField);
+        panel.add(loginButton);
+        panel.add(signupLabel);
+        panel.add(signupButton);
+        add(panel);
+
+        // Display the JFrame
+        setVisible(true);
+    }
+
+}
